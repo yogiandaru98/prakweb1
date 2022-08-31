10d145c326b5c5904f68b0d453f30eb3bf27330b
YOGI ANDARU <yogiandaru98@gmail.com>
2022-08-31 12:31:06 +0700

    web lanjut 1

---
+++ app/Views/pages/home.php
@@ -0,0 +1,6 @@
+<h3>
+    halaman home
+</h3>
+<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, cupiditate quas aliquam soluta provident voluptatem cum culpa consequuntur laudantium? Ullam laudantium esse voluptas, voluptate numquam praesentium hic minus enim accusamus.
+</p>
